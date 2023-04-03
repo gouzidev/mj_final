01049f80734043b4762056c52c6aa790cfd3b37d
gouzidev <gouzi.dev@gmail.com>
2023-04-03 11:26:47 +0000

    idk_what_that_is

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    Route::get('/', function () {
+        return view("welcome");
+    });
+    Route::get('home', function () {
+        return view("welcome");
+    });
 });
 
 Route::get('/dashboard', function () {
