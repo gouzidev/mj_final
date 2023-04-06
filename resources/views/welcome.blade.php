0863f1c342eea228955310f5ec762d73f2e0c0f4
Abderrahim chikhi <81624024+abderrahim00chikhi@users.noreply.github.com>
2023-04-06 23:55:20 +0000

    False

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
     @livewireStyles
 
 </head>
-<body class="p-2 md:p-0 flex-wrap dark:bg-gray-900 ">
+<body class="p-2 md:p-0 flex-wrap dark:bg-gray-800 ">
     @livewire('navbar')
     
     @livewire("app")
