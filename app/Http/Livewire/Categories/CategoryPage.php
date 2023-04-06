34484eeca61d98a2cc1a6eb4a6d9dbd56dffc7ac
gouzidev <gouzi.dev@gmail.com>
2023-04-06 13:37:42 +0000

    test

---
+++ app/Http/Livewire/Categories/CategoryPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Categories;
+
+use Livewire\Component;
+
+class CategoryPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.categories.category-page');
+    }
+}
--- resources/views/livewire/categories/category-name.blade.php
+++ resources/views/livewire/categories/category-name.blade.php
@@ -2,8 +2,8 @@
     require_once(resource_path("imports/limitString.php"))
 @endphp
 
-<li data-popover-target={{"popover-right".$category_index}} data-popover-placement="right">
-    <a href="#" class="flex items-center p-3 text-base text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<li data-popover-target={{"popover-right".$category->id}} data-popover-placement="right">
+    <a href="{{ route("category.show", $category->id) }}" class="flex items-center p-3 text-base text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
         <span class="flex-1 ml-3 whitespace-nowrap">{{limitString( $category->category_name, 30 )}}</span>
         <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
             0
@@ -12,7 +12,7 @@
 </li>
 
   
-<div data-popover id={{"popover-right".$category_index}} role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+<div data-popover id={{"popover-right".$category->id}} role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
     <div class="px-3 py-2">
         <p>{{$category->category_name}}</p>
     </div>
